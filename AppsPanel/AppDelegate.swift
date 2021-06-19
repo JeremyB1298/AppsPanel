85c0a736f63dae7448612f8603d1e7509c28f078
Jeremy  Bailly <jeremy.bailly@hotmail.com>
2021-06-19 10:26:45 +0200

    Init project

---
+++ AppsPanel/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AppsPanel
+//
+//  Created by Jeremy  Bailly  on 19/06/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
